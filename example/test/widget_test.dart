455e03d216e4ad3acf3e5a00ee2abb696e090083
LiggMax <wenzhouli06@gmail.com>
2026-08-19 21:26:28 +0800

    chore(example): 初始化示例项目

---
+++ example/test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_opencc_example/main.dart';
+
+void main() {
+  testWidgets('renders the example app', (WidgetTester tester) async {
+    await tester.pumpWidget(const OpenCCExampleApp());
+
+    expect(find.text('flutter_opencc example app'), findsOneWidget);
+  });
+}
